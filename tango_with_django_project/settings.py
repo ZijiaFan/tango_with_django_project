2259be54238792f331dcbdad6e99562d3ac1102e
Zijia Fan <2582033f@student.gla.ac.uk>
2021-07-30 13:14:18 +0100

    chapter10

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,7 +23,6 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'sq_z*m=yr)aiu$9lyzf@)&-!o(yjs=)9igwhuco$d7@a3jed=@'
-LOGIN_URL = '/rango/login/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
